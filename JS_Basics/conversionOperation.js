a2c2150d3d2e352fa64ce34d3509747c36716c4a
Nali0484 <Nali0484@conestogac.on.ca>
2025-07-02 18:12:30 -0400

    Created and worked on intro.js and conversionOperation.js

---
+++ JS_Basics/conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "56"
+//console.log(Number(score));
+
+let converetedSocre = Number(score);
+//console.log(typeof(onveretedSocre));
+
+let isloggedIn = false
+let randomString = "33abc"
+console.log(Number(randomString));
+
+/*
+"33" --> 33
+"" or null --> 0
+
+
+*/
---
+++ JS_Basics/intro.js
@@ -0,0 +1,36 @@
+//console.log ("Hello World")
+
+const accountID = 2345
+let accountEmail = "sain@google.com"
+var accountPassword = "password"
+accountCity = "Waterloo"
+let accountProvince;
+
+//console.log(accountProvince)
+
+//console.log(typeof(accountEmail))
+
+//console.log("User account IS is: "+accountID+ "and account Email is: "+accountEmail);
+//console.log('User account ID is ${accountID} and account Email is ${accountEmail}')
+
+console.log(3
+    +
+    3)
+let name = "Ahmad"
+let age = 18
+let isLogged = false
+let province
+
+console.log(typeof(province));
+
+/*
+string
+number
+boole
+undefined
+null
+*/
+
+console.log(typeof(undefined));
+
+console.log(typeof(null));
